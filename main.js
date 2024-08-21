a61d8928172e44e2ed98ea1666547672e32cf08d
Hussein <Hussein.alwahs02@gmail.com>
2024-08-21 09:57:07 +0300

    feat: create disple text and remove test

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ class List {
     #nameTest
     #statusTest
     #completeTest
-    constructor(nameTest, statusTest) {
+    id
+    constructor(id, nameTest, statusTest) {
+        this.id = id
         this.#nameTest = nameTest;
         this.#statusTest = statusTest;
         this.#completeTest = false;
@@ -35,14 +37,60 @@ let textTest = document.getElementById("textTest")
 let priority = document.getElementById("priority")
 let arrayTest = []
 let ul = document.getElementById("ul")
+let cunt = 0
 
 addList.addEventListener("click", () => {
+    let newTest = ""
     if (textTest.value !== "") {
-        let newTest = new List(textTest.value,priority.value);
+        cunt++;
+        newTest = new List(arrayTest.length, textTest.value, priority.value);
         //    console.log(document.getElementById("priority").value)
         arrayTest.push(newTest);
-    
-     console.log(arrayTest) 
-     
+
+        console.log(arrayTest)
+
     }
+    textTest.value = ""
+    display()
+
+    taskPriority(newTest.getstatusTest())
+
 })
+
+// disply the test 
+const display = () => {
+
+    let test = ""
+    arrayTest.forEach((item) => {
+
+        test += `<li>
+                    <p>${item.getNameTest()}</p> 
+                    <div>
+                        <input type="checkbox" id="myCheckbox" name="myCheckbox">
+                        <button id="delete" onclick=taskdelete(${item.id})>delete</button>
+                        <button id="edit">edit</button>
+                    </div>
+
+                </li>
+`
+
+
+        // console.log(  document.querySelector("li") )
+
+
+    }
+    )
+    ul.innerHTML = test;
+}
+
+// remove test 
+const taskdelete = (id) => {
+    const index = arrayTest.findIndex(item => item.id === id);
+    if (arrayTest.length !== -1) {
+        arrayTest.splice(index, 1)
+    }
+    display()
+
+
+
+}
