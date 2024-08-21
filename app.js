893a5c0623d9e4151f0cf8ccaacca009aed99c37
Hussein <Hussein.alwahs02@gmail.com>
2024-08-21 17:25:47 +0300

    feat: Work on arrangement

--- app.js
+++ app.js
@@ -1,48 +1,39 @@
-class TodoITask {
-  #nameTask;
-  #statusTask;
-  #completeTask;
-  constructor(nameTask, statusTask) {
-    this.#nameTask = nameTask;
-    this.#statusTask = statusTask;
-    this.#completeTask = false;
+class taskItem {
+  #taskName;
+
+  constructor(taskName) {
+    this.#taskName = taskName;
   }
   getNameTask() {
-    return this.#nameTask;
+    return this.#taskName;
   }
 }
 
 //Vareble in this code
-let addList = document.getElementById("addList");
-let taskValue = document.getElementById("taskValue");
-let priority = document.getElementById("priority");
-let tasks = [];
-let ul = document.getElementById("ul");
+const addListElement = document.getElementById("addList");
+const taskNameElement = document.getElementById("taskName");
+const taskPriorityElement = document.getElementById("priority");
+const tasks = [];
+const tasksListElement = document.getElementById("tasksList");
 
-addList.addEventListener("click", () => {
-  if (taskValue.value !== "") {
-    let newTask = new TodoITask(taskValue.value, priority.value);
+addListElement.addEventListener("click", () => {
+  if (taskNameElement.value !== "") {
+    let newTask = new taskItem(taskNameElement.value);
     tasks.push(newTask);
-    console.log(tasks)
-    renderList()
-    taskValue.value=""
+    console.log(tasks);
+    renderList();
+    taskNameElement.value = "";
   }
-
 });
 
-
 const renderList = () => {
-  ul.innerHTML = '';
+  tasksListElement.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
-
     const todoItemElement = document.createElement("li");
     const taskNameItem = document.createElement("p");
     todoItemElement.appendChild(taskNameItem);
-    taskNameItem.textContent = `${tasks[i].getNameTask()}`
-    ul.appendChild(todoItemElement);
-
-
+    taskNameItem.textContent = `${tasks[i].getNameTask()}`;
+    tasksListElement.appendChild(todoItemElement);
   }
-  console.log(ul)
-}
-
+  console.log(tasksListElement);
+};
