a0d5bf0f22f234c5ac649969fab4ed7c592d354f
Hussein <Hussein.alwahs02@gmail.com>
2024-08-21 15:36:55 +0300

    feat: Add the task to the render List, and the error has been fixed.

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-class List {
-  #nameTest;
-  #statusTest;
-  #completeTest;
-  constructor(nameTest, statusTest) {
-    this.#nameTest = nameTest;
-    this.#statusTest = statusTest;
-    this.#completeTest = false;
+class TodoITask {
+  #nameTask;
+  #statusTask;
+  #completeTask;
+  constructor(nameTask, statusTask) {
+    this.#nameTask = nameTask;
+    this.#statusTask = statusTask;
+    this.#completeTask = false;
   }
-  getNameTest() {
-    return this.#nameTest;
-  }
-  getstatusTest() {
-    return this.#statusTest;
-  }
-  getcompleteTest() {
-    return this.#completeTest;
-  }
-  setNameTest(nameTest) {
-    this.#nameTest = nameTest;
-  }
-  setStatusTes(setNameTest) {
-    this.#statusTest = setNameTest;
-  }
-  setCompleteTest(completeTest) {
-    this.#completeTest = completeTest;
+  getNameTask() {
+    return this.#nameTask;
   }
 }
 
 //Vareble in this code
 let addList = document.getElementById("addList");
-let textTest = document.getElementById("textTest");
+let taskValue = document.getElementById("taskValue");
 let priority = document.getElementById("priority");
-let arrayTest = [];
+let tasks = [];
 let ul = document.getElementById("ul");
 
 addList.addEventListener("click", () => {
-  if (textTest.value !== "") {
-    let newTest = new List(textTest.value, priority.value);
-    //    console.log(document.getElementById("priority").value)
-    arrayTest.push(newTest);
-    debugger;
-    if (newTest.getstatusTest() === "High") {
-      console.log("High");
-    }
-    console.log(arrayTest);
+  if (taskValue.value !== "") {
+    let newTask = new TodoITask(taskValue.value, priority.value);
+    tasks.push(newTask);
+    console.log(tasks)
+    renderList()
   }
+
 });
+
+
+const renderList = () => {
+  ul.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+
+    const todoItemElement = document.createElement("li");
+    const taskNameItem = document.createElement("p");
+    todoItemElement.appendChild(taskNameItem);
+    taskNameItem.textContent = `${tasks[i].getNameTask()}`
+    ul.appendChild(todoItemElement);
+
+
+  }
+  console.log(ul)
+}
+
