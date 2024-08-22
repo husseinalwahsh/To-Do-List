d1e7868eaa3c4cda0f6062862867c24e59729f66
Hussein <Hussein.alwahs02@gmail.com>
2024-08-22 16:50:46 +0300

    feat:  function taskStatus

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 class TaskItem {
   #taskName;
+  #taskStatus;
 
-  constructor(taskName) {
+  constructor(taskName, taskStatus) {
     this.#taskName = taskName;
+    this.#taskStatus = taskStatus;
   }
   getNameTask() {
     return this.#taskName;
+  }
+  getTaskStatus() {
+    return this.#taskStatus;
   }
 }
 
@@ -13,12 +18,13 @@ class ToDoList {
   constructor() {
     this.tasks = [];
   }
-  addTask(taskName) {
-    const newTask = new TaskItem(taskName);
+  addTask(taskName, taskStatus) {
+    const newTask = new TaskItem(taskName, taskStatus);
     this.tasks.push(newTask);
     console.log(this.tasks);
     this.renderList();
     taskNameElement.value = "";
+    this.setTaskPriority();
   }
   renderList() {
     tasksListElement.innerHTML = "";
@@ -42,6 +48,18 @@ class ToDoList {
     this.tasks.splice(index, 1);
     this.renderList();
   }
+  setTaskPriority() {
+    for (let i = 0; i < this.tasks.length; i++) {
+      const todoItemElement = document.querySelectorAll("li")[i];
+      if (this.tasks[i].getTaskStatus() === "high") {
+        todoItemElement.style.background = "rgba(255, 0, 0, 0.493)";
+      } else if (this.tasks[i].getTaskStatus() === "medium") {
+        todoItemElement.style.background = "rgba(246, 255, 0, 0.795)";
+      } else {
+        todoItemElement.style.background = "rgba(30, 255, 0, 0.537)";
+      }
+    }
+  }
 }
 
 //Vareble in this code
@@ -52,8 +70,9 @@ const toDoList = new ToDoList();
 const tasksListElement = document.getElementById("tasksList");
 addListElement.onclick = () => {
   const taskName = taskNameElement.value;
+  const taskStatus = taskPriorityElement.value;
   if (taskName !== "") {
-    toDoList.addTask(taskName);
+    toDoList.addTask(taskName, taskStatus);
   } else {
     alert("Please enter a task name.");
   }
