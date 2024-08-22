9683f07c7619a1d431745addb21a98bafac7c7a7
Hussein <Hussein.alwahs02@gmail.com>
2024-08-22 15:44:19 +0300

    feat: change on somethin like remove value,delete element div and add style pointer on button delete

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ class ToDoList {
     for (let i = 0; i < this.tasks.length; i++) {
       const todoItemElement = document.createElement("li");
       const taskNameItem = document.createElement("p");
-      const contunerButton= document.createElement("div");
       const deleteTaskButton = document.createElement("button");
       deleteTaskButton.id="delete"
       deleteTaskButton.textContent = "Delete";
@@ -33,8 +32,7 @@ class ToDoList {
        this.deleteTask(i)
       }
       todoItemElement.appendChild(taskNameItem);
-      contunerButton.appendChild(deleteTaskButton);
-      todoItemElement.appendChild(contunerButton);
+      todoItemElement.appendChild(deleteTaskButton);
       taskNameItem.textContent = `${this.tasks[i].getNameTask()}`;
       tasksListElement.appendChild(todoItemElement);
     }
@@ -53,7 +51,6 @@ const taskNameElement = document.getElementById("taskName");
 const taskPriorityElement = document.getElementById("priority");
 const toDoList = new ToDoList();
 const tasksListElement = document.getElementById("tasksList");
-const taskDeleteElement = document.getElementById("delete");
 addListElement.onclick = () => {
   const taskName = taskNameElement.value;
   if (taskName !== "") {
