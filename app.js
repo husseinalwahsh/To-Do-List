dfdd7aca29bafd4b95861e787bda56ddf1ac12af
Hussein <Hussein.alwahs02@gmail.com>
2024-08-22 15:08:47 +0300

    feat: create function delete taskElement

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class TaskItem {
   #taskName;
-
+ 
   constructor(taskName) {
     this.#taskName = taskName;
   }
@@ -25,12 +25,26 @@ class ToDoList {
     for (let i = 0; i < this.tasks.length; i++) {
       const todoItemElement = document.createElement("li");
       const taskNameItem = document.createElement("p");
+      const contunerButton= document.createElement("div");
+      const deleteTaskButton = document.createElement("button");
+      deleteTaskButton.id="delete"
+      deleteTaskButton.textContent = "Delete";
+      deleteTaskButton.onclick= () =>{
+       this.deleteTask(i)
+      }
       todoItemElement.appendChild(taskNameItem);
+      contunerButton.appendChild(deleteTaskButton);
+      todoItemElement.appendChild(contunerButton);
       taskNameItem.textContent = `${this.tasks[i].getNameTask()}`;
       tasksListElement.appendChild(todoItemElement);
     }
     console.log(tasksListElement);
   }
+  deleteTask(index){
+    this.tasks.splice(index,1)
+    this.renderList()
+  }
+
 }
 
 //Vareble in this code
@@ -39,7 +53,7 @@ const taskNameElement = document.getElementById("taskName");
 const taskPriorityElement = document.getElementById("priority");
 const toDoList = new ToDoList();
 const tasksListElement = document.getElementById("tasksList");
-
+const taskDeleteElement = document.getElementById("delete");
 addListElement.onclick = () => {
   const taskName = taskNameElement.value;
   if (taskName !== "") {
@@ -48,3 +62,4 @@ addListElement.onclick = () => {
     alert("Please enter a task name.");
   }
 };
+
