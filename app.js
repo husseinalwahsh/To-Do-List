20fca5421d3da450d99f072d376ca551a7e8eae7
Hussein <Hussein.alwahs02@gmail.com>
2024-08-26 13:54:34 +0300

    create function filter and edit

--- app.js
+++ app.js
@@ -11,53 +11,95 @@ class TaskItem {
   getTaskPriority() {
     return this.#taskPriority;
   }
+  setNameTask(taskName){
+    this.#taskName=taskName;
+  }
 }
 
 class ToDoList {
   constructor() {
     this.tasks = [];
+    this.currentFilter = "all";
+    this.init();
+  }
+  init() {
+    const filterSelectElement = document.getElementById("filterByPriority");
+    filterSelectElement.addEventListener("change", (event) => {
+      this.currentFilter = event.target.value;
+      this.renderList();
+    });
   }
   addTask(taskName, taskPriority) {
     const newTask = new TaskItem(taskName, taskPriority);
+    // this.tasks.push(newTask);
     this.tasks.push(newTask);
-    console.log(this.tasks);
+
     this.renderList();
     taskNameElement.value = "";
   }
   renderList() {
     const tasksListElement = document.getElementById("tasksList");
     tasksListElement.innerHTML = "";
+    // this.currentFilter === "all";
+    // ? this.tasks
+    // : this.tasks.filter(
+    //     (task) => task.getTaskPriority() === this.currentFilter,
+    //   );
     for (let i = 0; i < this.tasks.length; i++) {
-      const todoItemElement = document.createElement("li");
-      const taskNameItem = document.createElement("p");
-      const deleteTaskButton = document.createElement("button");
-      const checkboxElement = document.createElement("input");
-      checkboxElement.type = "checkbox";
-      deleteTaskButton.id = "delete";
-      deleteTaskButton.textContent = "Delete";
-      deleteTaskButton.onclick = () => {
-        this.deleteTask(i);
-      };
-      todoItemElement.appendChild(taskNameItem);
-      todoItemElement.appendChild(checkboxElement);
-      todoItemElement.appendChild(deleteTaskButton);
-      taskNameItem.textContent = `${this.tasks[i].getNameTask()}`;
+      if (
+        this.currentFilter === "all" ||
+        this.tasks[i].getTaskPriority() === this.currentFilter
+      ) {
+        const todoItemElement = document.createElement("li");
+        const divElement = document.createElement("div");
+        const taskNameItem = document.createElement("p");
+        const deleteTaskButton = document.createElement("button");
+        const editTaskButton = document.createElement("button");
+        const checkboxElement = document.createElement("input");
+        taskNameItem.contenteditable = "true";
+        checkboxElement.type = "checkbox";
+        deleteTaskButton.id = "delete";
+        editTaskButton.textContent = "Edit";
+        deleteTaskButton.textContent = "Delete";
+        deleteTaskButton.onclick = () => {
+          this.deleteTask(i);
+        };
+        todoItemElement.appendChild(taskNameItem);
+        todoItemElement.appendChild(checkboxElement);
+        divElement.appendChild(deleteTaskButton);
+        divElement.appendChild(editTaskButton);
+        todoItemElement.appendChild(divElement);
+        taskNameItem.textContent = `${this.tasks[i].getNameTask()}`;
 
-      tasksListElement.appendChild(todoItemElement);
-      checkboxElement.addEventListener("change", (event) => {
-        const todoItemElement = document.querySelectorAll("li")[i];
-        //  todoItemElement.style.textDecoration="underline";
-        if (event.target.checked) {
-          todoItemElement.classList.add("complete");
-        } else {
-          todoItemElement.classList.remove("complete");
-        }
-      });
-      todoItemElement.classList.add(this.tasks[i].getTaskPriority());
+        tasksListElement.appendChild(todoItemElement);
+        checkboxElement.addEventListener("change", (event) => {
+          const todoItemElement = document.querySelectorAll("li")[i];
+          //  todoItemElement.style.textDecoration="underline";
+          if (event.target.checked) {
+            todoItemElement.classList.add("complete");
+          } else {
+            todoItemElement.classList.remove("complete");
+          }
+        });
+        todoItemElement.classList.add(this.tasks[i].getTaskPriority());
+        editTaskButton.onclick = () => {
+          if (editTaskButton.textContent === "Edit") {
+            taskNameItem.contentEditable = "true";
+            taskNameItem.focus();
+            editTaskButton.textContent = "Save";
+          } else {
+            taskNameItem.contentEditable = "false";
+            this.tasks[i].setNameTask(taskNameItem.textContent);
+            editTaskButton.textContent = "Edit";
+            this.renderList();
+          }
+        };
+      }
     }
   }
   deleteTask(index) {
     this.tasks.splice(index, 1);
+
     this.renderList();
   }
 }
